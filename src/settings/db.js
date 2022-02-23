6b54e17fde7a3d63cee4fd37f890866d48ee2e6b
Lina Virginia <linavm@unicauca.edu.co>
2022-02-22 21:51:53 -0500

    conexión base de datos online

--- src/settings/db.js
+++ src/settings/db.js
@@ -1,21 +1,26 @@
 //conexión a base de datos
 
 const conn = require('mongoose');
-//const Dispositivo = require('./dispositivo')
-    //para conexión en la nube
-    // const user = 'grupoWebLab4';
-    // const password= 'grupoweb2021';
-    // const dbName = 'laboratorioIV';
-    // const uri = `mongodb+srv://${user}:${password}@cluster0.jmj5o.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+    // para conexión en la nube
+    const user = 'grupoWebLab4';
+    const password= 'grupoweb2021';
+    const dbName = 'laboratorioIV';
+    const uri = `mongodb+srv://${user}:${password}@cluster0.jmj5o.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
     // conn.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     //   .then(()=> console.log('conectado a mongodb')) 
     //   .catch(e => console.log('error de conexión', e))
+        conn.connect(uri,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+            .then(data => console.log('conectado a mongodb'))
+            .then(err => console.log(err))
 
-    //conexión a base de datos local
-conn.connect('mongodb://localhost:27017/laboratorioIV',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(data => console.log('conectado a mongodb'))
-    .then(err => console.log(err))
+// conexión a base de datos local
+// conn.connect('mongodb://localhost:27017/laboratorioIV',{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+//     .then(data => console.log('conectado a mongodb'))
+//     .then(err => console.log(err))
